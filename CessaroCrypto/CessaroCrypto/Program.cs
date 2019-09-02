2649735e75ec417770332369a208bf80d3f3c259
kamynin.2015 <kamynin.2015@PMI-208B-12>
2019-09-02 16:21:42 +0700

    eee

---
+++ CessaroCrypto/CessaroCrypto/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CessaroCrypto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, char> dict = new Dictionary<char, char>();
+            string key = File.ReadAllText(args[0]);
+            for (char i = 'а'; i <= 'я'; i++)
+            {
+                dict[i] = key[i - 'а'];
+            }
+
+            string textToTranslate = File.ReadAllText(args[1]);
+            textToTranslate = textToTranslate.ToLower();
+            textToTranslate = new string(textToTranslate.Where(x => x >='а' && x <= 'я' ).ToArray<char>());
+            string newText = "";
+            for (int i = 0; i < textToTranslate.Length; i++)
+            {
+                newText += dict[textToTranslate[i]];
+            }
+
+            File.WriteAllText(args[2], newText);
+        }
+    }
+}
---
+++ KeyGenerator/KeyGenerator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+namespace KeyGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random seed = new Random();
+            List <char> dict = new List<char> ( "абвгдеёжзийкламнопрстуфхцчшщъыьэюя");
+            for (int i = 33; i >= 0; i--)
+            {
+                int j = seed.Next(i);
+                char tmp = dict[j];
+                dict[j] = dict[i];
+                dict[i] = tmp;
+            }
+            File.WriteAllText(args[0] ,$"{new string(dict.ToArray())}");
+        }
+    }
+}
