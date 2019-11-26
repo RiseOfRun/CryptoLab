39ee7512c8469bdebe36eb52dd71ba76d203f2be
Kim Stanislav <kimevgeniy61@gmail.com>
2019-11-26 20:46:48 +0300

    WAAAAAAH

---
+++ cry3/cry3/Certif.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography;
+using System.Security.Cryptography.Pkcs;
+
+namespace cry3
+{
+    class Certif
+    {
+
+        private RSACryptoServiceProvider key;
+        public Certif(int len)
+        {
+            key = new RSACryptoServiceProvider(len);
+        }
+
+        public void CreateCertificate(string name)
+        {
+            CertificateRequest req = new CertificateRequest("CN=Стас и Антон", key, 
+                HashAlgorithmName.SHA256,
+                RSASignaturePadding.Pkcs1);
+
+            req.CertificateExtensions.Add(new X509BasicConstraintsExtension(false, false, 0, false));
+            req.CertificateExtensions.Add(new X509KeyUsageExtension(
+                X509KeyUsageFlags.DigitalSignature | X509KeyUsageFlags.NonRepudiation, false));
+            req.CertificateExtensions.Add(new X509EnhancedKeyUsageExtension(
+                new OidCollection
+                {
+                    new Oid("2.6.1.1.2.0.1.9")
+                },
+                true));
+            req.CertificateExtensions.Add(new X509SubjectKeyIdentifierExtension(req.PublicKey, false));
+            X509Certificate2 certif =  req.CreateSelfSigned(DateTimeOffset.UtcNow.AddDays(-1),
+                DateTimeOffset.UtcNow.AddYears(50));
+
+            File.WriteAllBytes(name + ".cer", certif.Export(X509ContentType.Cert));
+            certif.PrivateKey = key;
+            File.WriteAllBytes(name + ".pem", certif.Export(X509ContentType.Pkcs12, "Fynjy123"));
+
+            //key.ExportParameters(true).D;
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ cry3/cry3/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+using System;
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography.Pkcs;
+using System.Security.Cryptography;
+using System.Security.Permissions;
+namespace cry3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Certif s = new Certif(1024);
+            s.CreateCertificate("Anton");*/
+            /*X509Certificate2 cert = new X509Certificate2("Anton.pem", "Fynjy123");
+            Console.WriteLine($"{cert.HasPrivateKey}");*/
+
+
+            
+            
+
+
+
+        }
+    }
+}
