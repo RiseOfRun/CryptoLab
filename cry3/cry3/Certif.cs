ca984dc730a2686db9328d59d76a26315fe2c958
Kim Stanislav <kimevgeniy61@gmail.com>
2019-12-08 13:00:49 +0300

    FIX

--- cry3/cry3/Certif.cs
+++ cry3/cry3/Certif.cs
@@ -15,6 +15,7 @@ namespace cry3
         private const string PASS = "Fynjy123";
         private const string AUTH = "Стас и Антон";
         private RSACryptoServiceProvider key;
+
         public Certif(int len)
         {
             key = new RSACryptoServiceProvider(len);
@@ -22,11 +23,11 @@ namespace cry3
 
         public void CreateCertificate(string name)
         {
-            CertificateRequest req = new CertificateRequest("CN=" + AUTH, key, 
+            CertificateRequest req = new CertificateRequest("CN=" + AUTH, key,
                 HashAlgorithmName.SHA256,
                 RSASignaturePadding.Pkcs1);
             req.CertificateExtensions.Add(new X509SubjectKeyIdentifierExtension(req.PublicKey, false));
-            X509Certificate2 certif =  req.CreateSelfSigned(DateTimeOffset.UtcNow.AddDays(-1),
+            X509Certificate2 certif = req.CreateSelfSigned(DateTimeOffset.UtcNow.AddDays(-1),
                 DateTimeOffset.UtcNow.AddYears(50));
             File.WriteAllBytes(name + ".cer", certif.Export(X509ContentType.Cert));
             certif.PrivateKey = key;
@@ -36,31 +37,49 @@ namespace cry3
         public void SignFile(string nameCert, string nameFile)
         {
             X509Certificate2 cert = new X509Certificate2(nameCert, PASS);
-            SHA256CryptoServiceProvider hasher = new SHA256CryptoServiceProvider();        
+            SHA256CryptoServiceProvider hasher = new SHA256CryptoServiceProvider();
             byte[] file = File.ReadAllBytes(nameFile);
             byte[] hash = hasher.ComputeHash(file);
-            RSACryptoServiceProvider csp = cert.PrivateKey as RSACryptoServiceProvider;            
+            RSACryptoServiceProvider csp = cert.PrivateKey as RSACryptoServiceProvider;
             byte[] certHash = csp.SignData(file, new SHA256CryptoServiceProvider());
             File.WriteAllBytes(nameFile + ".sgn", certHash);
             File.WriteAllBytes(nameFile + ".sha256", hash);
         }
 
-        public bool Validate(string nameCert, string sgnFilePath, string hashpath)
+
+
+        public bool ValidateHash(string nameCert, string sgnFilePath, string hashpath)
         {
-            X509Certificate2 cert = new X509Certificate2(nameCert, PASS);
-            byte[] file = File.ReadAllBytes(sgnFilePath);
+            X509Certificate2 cert = new X509Certificate2(nameCert);
+            byte[] sgnfile = File.ReadAllBytes(sgnFilePath);
 
             byte[] hashfile = File.ReadAllBytes(hashpath);
+            bool flag = false;
             RSACryptoServiceProvider csp = cert.PublicKey.Key as RSACryptoServiceProvider;
-            csp.VerifyHash(hashfile, file, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
-            csp.VerifyHash(hashfile, file, HashAlgorithmName.SHA384, RSASignaturePadding.Pkcs1);
-            csp.VerifyHash(hashfile, file, HashAlgorithmName.SHA512, RSASignaturePadding.Pkcs1);
-            csp.VerifyHash(hashfile, file, HashAlgorithmName.MD5, RSASignaturePadding.Pkcs1);
-            csp.VerifyHash(hashfile, file, HashAlgorithmName.SHA1, RSASignaturePadding.Pkcs1);
-            return true;
+            try { flag = flag || csp.VerifyHash(hashfile, sgnfile, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1); } catch { };
+            try { flag = flag || csp.VerifyHash(hashfile, sgnfile, HashAlgorithmName.SHA384, RSASignaturePadding.Pkcs1); } catch { };
+            try { flag = flag || csp.VerifyHash(hashfile, sgnfile, HashAlgorithmName.SHA512, RSASignaturePadding.Pkcs1); } catch { };
+            try { flag = flag || csp.VerifyHash(hashfile, sgnfile, HashAlgorithmName.MD5, RSASignaturePadding.Pkcs1); } catch { };
+            try { flag = flag || csp.VerifyHash(hashfile, sgnfile, HashAlgorithmName.SHA1, RSASignaturePadding.Pkcs1); } catch { };
+            return flag;
         }
 
-      
+
+        public bool ValidateFile(string nameCert, string sgnFilePath, string path)
+        {
+            X509Certificate2 cert = new X509Certificate2(nameCert);
+            byte[] sgnfile = File.ReadAllBytes(sgnFilePath);
+
+            byte[] file = File.ReadAllBytes(path);
+            bool flag = false;
+            RSACryptoServiceProvider csp = cert.PublicKey.Key as RSACryptoServiceProvider; 
+            try { flag = flag || csp.VerifyData(file, sgnfile, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1); } catch { };
+            try { flag = flag || csp.VerifyData(file, sgnfile, HashAlgorithmName.SHA384, RSASignaturePadding.Pkcs1); } catch { };
+            try { flag = flag || csp.VerifyData(file, sgnfile, HashAlgorithmName.SHA512, RSASignaturePadding.Pkcs1); } catch { };
+            try { flag = flag || csp.VerifyData(file, sgnfile, HashAlgorithmName.MD5, RSASignaturePadding.Pkcs1); } catch { };
+            try { flag = flag || csp.VerifyData(file, sgnfile, HashAlgorithmName.SHA1, RSASignaturePadding.Pkcs1); } catch { };
+            return flag;
+        }
 
 
 
